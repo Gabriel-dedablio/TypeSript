6072dc7a19e9e890f9f76e8718cbbbc922ffd014
Gabriel-dedablio <gabriel-de-olliveira@hotmail.com>
2023-02-18 11:11:27 -0300

    Aulas 1 e 2

---
+++ Ts para iniciantes/aula1/script.js
@@ -0,0 +1,22 @@
+
+const frase = 'Front End';
+const total = 100.05;
+const empresas = ['Apple', 'Microsoft'];
+
+console.log(frase.toLocaleLowerCase());
+
+const t = total.toFixed();
+
+console.log(t+10);
+
+console.log(empresas.map(empresas => empresas.toLocaleLowerCase()));
+
+
+const body = document.body;
+
+body.style.background = "#000";
+
+const button = document.querySelector('button');
+
+const operacao = 100 + 200;
+console.log(operacao);
