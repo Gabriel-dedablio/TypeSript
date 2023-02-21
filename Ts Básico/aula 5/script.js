222fc6fbdaf9e807b5ea4410e008f0b5b1de9c11
Gabriel-dedablio <gabriel-de-olliveira@hotmail.com>
2023-02-21 11:10:47 -0300

    Criando type

---
+++ Ts Básico/aula 5/script.js
@@ -0,0 +1,21 @@
+"use strict";
+function preencherDados(dados) {
+    document.body.innerHTML += `
+    <div
+      <h2>${dados.nome}</h2>
+      <h2>R$${dados.preco}</h2>
+      <h2>Inclui teclado: ${dados.teclado ? 'sim' : 'Não'}</h2>
+    </div>
+  `;
+}
+const computador = {
+    nome: "Computador",
+    preco: 2020,
+    teclado: false,
+};
+preencherDados(computador);
+preencherDados({
+    nome: "Notebook",
+    preco: 2020,
+    teclado: true,
+});
